142723048cad669c85950becd424133892b36d27
andreybozhkov <bozhkov.andrey@gmail.com>
2017-07-18 22:56:54 +0300

    ADDED processing first command line

--- script/consoleReader.js
+++ script/consoleReader.js
@@ -1,17 +1,34 @@
 (function () {
+    let arr = [];
+    let initialized = false;
+    let index;
     let terminal = $('#terminal');
     let input = $('#console');
     $('#submit').click(submit);
-    
+
     input.keypress(
     function(e) {
         if (e.which === 13) {
             submit();
         }
     });
-    
+
     function submit() {
         let commandToken = input.val().split(' ').filter(e => e !== '');
 
+        switch (commandToken[0]){
+            default:
+                if (initialized) {
+                    print("Error: invalid command");
+                }
+            }
     }
+
+    if (!initialized) {
+        arr = commandToken.slice(0);
+        initialized = true;
+        terminal.value += arr.join(' ') + '\n';
+    }
+
+    input.value = '';
 })();
